0277ee15fd6a548509d4dab6c3d1123040c3eed1
moljz <90883549+moljz@users.noreply.github.com>
2023-08-13 20:03:29 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,4 +1,3 @@
-//This function will receive two numbers and should return the addition of those two numbers.
-function add(num1, num2) {
-  return num1 + num2;
+function calculateNumbers(Var1, Var2) {
+    return Var1 + Var2;
 }
